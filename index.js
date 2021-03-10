50bf1503cfde463ef9ca36fa0eb195ae45dbadd0
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-09 19:08:52 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
